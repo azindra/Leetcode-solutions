1f4b5d547d0ee8a6e109c48dd1eeba04ddec80e4
Ashika <siasia20050412@gmail.com>
2026-02-24 17:16:16 +0530

    swap..array..join string

---
+++ 151.reverse-words-in-a-string.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=151 lang=java
+ *
+ * [151] Reverse Words in a String
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseWords(String s) {
+        String[] words=s.trim().split("\\s+");
+        int left =0;
+        int right=words.length-1;
+        while(left<right){
+            String temp = words[left];
+            words[left]=words[right];
+            words[right]=temp;
+            left++;
+            right--;
+        }
+        return String.join(" ", words);
+
+        
+    }
+}
+// @lc code=end
+
