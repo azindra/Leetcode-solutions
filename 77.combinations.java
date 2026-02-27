ca0c04e5922f476e1332c2d1dc6cc87222fa86cb
Ashika <siasia20050412@gmail.com>
2026-02-27 16:05:40 +0530

    add

---
+++ 77.combinations.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=77 lang=java
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+import java.util.*;
+ class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(1, n, k, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int n, int k,
+                           List<Integer> temp,
+                           List<List<Integer>> result) {
+
+        // BASE CASE
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+
+            temp.add(i);                 
+
+            backtrack(i + 1, n, k, temp, result);  
+
+            temp.remove(temp.size() - 1); 
+        }
+    }
+}
+// @lc code=end
+
