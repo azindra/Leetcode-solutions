1b2e9fefaea255e2db7fb3844945671ba42f3a52
Ashika <siasia20050412@gmail.com>
2026-02-27 15:56:46 +0530

    backtracking

---
+++ 39.combination-sum.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=39 lang=java
+ *
+ * [39] Combination Sum
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        backtrack(candidates,target,0,new ArrayList<>(),result);
+        return result;
+        
+    }
+    private void backtrack(int [] candidates,int target,int start,List<Integer> temp ,List<List<Integer>> result){
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        if(target<0){
+            return;
+
+        }
+        for(int i = start;i<candidates.length;i++){
+            temp.add(candidates[i]);
+            backtrack(candidates, target-candidates[i], i, temp, result);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
+// @lc code=end
+
