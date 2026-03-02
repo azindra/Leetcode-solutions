60691511db68a2b821c88c42eb958074486c3d61
Ashika <siasia20050412@gmail.com>
2026-03-02 23:13:01 +0530

    add

---
+++ Solution.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;          // 1 step
+            fast = fast.next.next;    // 2 steps
+
+            if (slow == fast) {
+                return true;          // cycle detected
+            }
+        }
+        return false;                 // no cycle
+    }
+}
+// @lc code=end
+
