823bda3bdd1808b668ce332e2d010f58248f5857
Ashika <siasia20050412@gmail.com>
2026-03-18 22:49:06 +0530

    loop

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentsum =nums[0];
+        int maxsum = nums[0];
+        for(int i =1;i<nums.length;i++){
+            currentsum=Math.max(nums[i],currentsum+nums[i]);
+            maxsum= Math.max(maxsum, currentsum);
+        }
+        return maxsum;
+    }
+}
+// @lc code=end
+
